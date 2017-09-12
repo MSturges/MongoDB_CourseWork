0330d9781a410a57edeeb3446765c187b705d1d5
MSturges <Sturges3232@gmail.com>
2017-09-11 18:34:56 -0600

    mongo operators section complete

--- 01_users/src/user.js
+++ 01_users/src/user.js
@@ -11,7 +11,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Name is required.'],
+    validate: {
+      validator: (name) => name.length > 2 ,
+      message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
   },
   postCount: Number
 });
--- 01_users/test/validation_test.js
+++ 01_users/test/validation_test.js
@@ -3,10 +3,35 @@ const User = require('../src/user');
 
 describe('validating record', () => {
 
-  it('requires a username', () => {
+  it('Requires a Username', () => {
     const user = new User( { name: undefined });
     const validationResult = user.validateSync();
+    const  { message } = validationResult.errors.name
 
+    assert(message === 'Name is required.');
+
+  });
+
+  it('requires a user\`s name longer than 2 characters', (done) => {
+    const user = new User ( { name: 'al' });
+    // Synchronous
+    const validationResult = user.validateSync();
+    const  { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters.');
+    done();
+  });
+
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({name: 'Al'});
+    user.save()
+      .catch((validationResult) => {
+
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
   });
 
 });
